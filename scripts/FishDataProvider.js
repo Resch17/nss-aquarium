2e9c8a24e783c4c22dffc36df3a44d356c5daed5
Aaron Resch <aaronmresch@gmail.com>
2020-11-17 11:41:56 -0600

    added FishDataProvider.js and populated with fish from index.html

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,52 @@
+const fishCollection = [
+  {
+    species: "Goldfish",
+    name: "Tom",
+    length: 3,
+    location: "Neighbor's backyard pond",
+    diet: "Cheetos",
+    image: {
+      url: "./images/goldfish.jpg"
+    }
+  },
+  {
+    species: "Blue Tang",
+    name: "Ghostfish Killah",
+    length: 5,
+    location: "Staten Island, NY",
+    diet: "Whack MCs",
+    image: {
+      url: "./images/blueTang.jpg"
+    }
+  },
+  {
+    species: "Clownfish",
+    name: "Bozo",
+    length: 4,
+    location: "Great Barrier Reef",
+    diet: "Fish food? idk",
+    image: {
+      url: "./images/clownfish.jpg"
+    }
+  },
+  {
+    species: "Chinese Algae Eater",
+    name: "Franz",
+    length: 10,
+    location: "Yangtze River",
+    diet: "Algae",
+    image: {
+      url: "./images/algae.jpg"
+    }
+  },
+  {
+    species: "Lionfish",
+    name: "Aslan",
+    length: 16,
+    location: "Perth, Australia",
+    diet: "FISH",
+    image: {
+      url: "./images/lionfish.jpg"
+    }
+  }
+]
